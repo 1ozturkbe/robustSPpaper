b0b25d7a43953925f06336324fd7eea61b04574d
1ozturkbe <1ozturkbe@gmail.com>
2018-07-20 15:53:14 +0300

    Getting closer.

--- code/SimPleAC.py
+++ code/SimPleAC.py
@@ -26,18 +26,22 @@ subs = {
 
 # Putting in objectives and associated substitutions
 # in a dictionary
-objectives = {#m['W_{f_m}'] :                      {'name': 'Total fuel', 'added': {}, 'removed': {}},
-              #m['C_m']*m['t_m'] :                 {'name': 'Time cost', 'added': {}, 'removed': {}},
+objectives = {m['W_{f_m}'] :                      {'name': 'Total fuel', 'added': {}, 'removed': {}},
+              m['C_m']*m['t_m'] :                 {'name': 'Time cost', 'added': {}, 'removed': {}},
               #m['V_{min_m}'] :                    {'added': {}, 'removed': [m['V_{min_m}']]},
               m['W']:                             {'name': 'Takeoff weight', 'added': {}, 'removed': {}},
               m['A']:                             {'name': 'Aspect ratio', 'added': {}, 'removed': {}},
-              m['W_e']:                           {'name': 'Empty weight', 'added': {}, 'removed': {}},
+              m['W_e']:                           {'name': 'Engine weight', 'added': {}, 'removed': {}},
               m['W']/m['S']:                      {'name': 'Wing loading', 'added': {}, 'removed': {}},
-              #m['W_{f_m}']*units('1/N')+m['C_m']*m['t_m'] :    {'name': 'Total cost', 'added': {}, 'removed': {}},
+              m['W_{f_m}']+m['C_m']*m['t_m']*units('N') : {'name': 'Total cost', 'added': {}, 'removed': {}},
               }
 models = {}
 solutions = {}
-
+minimizer=1
+for i in objectives.iterkeys():
+    if i.unitstr() == 'N':
+        minimizer = minimizer*i
+minimizer = 10**-15*minimizer
 for i in objectives.iterkeys():
     #m = Mission(a, 4)
     #substitutions = {}
@@ -45,17 +49,26 @@ for i in objectives.iterkeys():
     #substitutions.update(objectives[i]['added'])
     #substitutions = removesubs(substitutions,objectives[i]['removed'])
     m.substitutions.update(subs)
-    m = Model(i, Bounded(m))
+    try:
+        m = Model(i+minimizer/minimizer.units*i.units, Bounded(m))
+    except:
+        m = Model(i+minimizer/minimizer.units, Bounded(m))
     models[i] = m
     solutions[i] = m.localsolve(verbosity=2)
 
 # Generating data amenable to radar plotting
 data =[]
 data.append([objectives[j]['name'] for j in objectives.iterkeys()])
+maxesindata = np.zeros(len(objectives))
 for i in objectives.iterkeys():
-    case = objectives[i]['name'] + ' optimized'
+    case = objectives[i]['name']
     caseData = []
-    caseData = [mag(solutions[i](j)) for j in objectives.iterkeys()]
+    count = 0
+    for j in objectives.iterkeys():
+        caseData.append(mag(solutions[i](j)))
+        if mag(solutions[i](j)) >= maxesindata[count]:
+            maxesindata[count] = mag(solutions[i](j))
+            count +=1
     data.append((case, [caseData]))
 
 # Plotting
@@ -63,19 +76,18 @@ N = len(solutions)
 theta = radar_factory(N, frame='polygon')
 spoke_labels = data.pop(0)
 
-fig, axes = plt.subplots(figsize=(7,7), nrows=2, ncols=2,
+fig, axes = plt.subplots(figsize=(7,7), nrows=4, ncols=2,
                          subplot_kw=dict(projection='radar'))
 fig.subplots_adjust(wspace=0.25, hspace=0.20, top=0.85, bottom=0.05)
 
 colors = ['b', 'r', 'g', 'm', 'y', 'c', 'o']
 
-# Plot the four cases from the example data on separate axes
 for ax, (title, case_data) in zip(axes.flatten(), data):
-    ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
+    #ax.set_rgrids([0.2, 0.4, 0.6, 0.8])
     ax.set_title(title, weight='bold', size='medium', position=(0.5, 1.1),
                  horizontalalignment='center', verticalalignment='center')
     for d, color in zip(case_data, colors):
-        ax.plot(theta, d, color=color)
+        ax.plot(theta, d/maxesindata, color=color)
         ax.fill(theta, d, facecolor=color, alpha=0.25)
     ax.set_varlabels(spoke_labels)
 
@@ -90,6 +102,4 @@ legend = ax.legend(labels, loc=(0.9, .95),
 #          size='large')
 
 plt.show()
-
-
 
