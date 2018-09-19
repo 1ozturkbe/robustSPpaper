afda4c75e4560547bf781d7cfdee2042f7e84919
1ozturkbe <1ozturkbe@gmail.com>
2018-09-19 10:32:18 -0400

    quick_run only works with twoTerm = False (Linear Perturbations).

--- code/quick_run.py
+++ code/quick_run.py
@@ -1,14 +1,15 @@
 from robust.robust import RobustModel
+from SimPleAC_setup import SimPleAC_setup
 from robust.signomial_simple_wing.models import simple_ac
 from robust.simulations.simulate import generate_model_properties
 from robust.robust_gp_tools import RobustGPTools
 
-from SimPleAC_setup import SimPleAC_setup
-
 model, subs = SimPleAC_setup()  # the SimPleAC model
 model_solution, _, _, directly_uncertain_vars_subs = generate_model_properties(model, 1, 1)  # solving the SimPleAC
 # and generating a realization of the uncertain parameters
-robust_model = RobustModel(model, 'box')  # generates the robust model
+# robust_model = RobustModel(model, 'box')  # generates the robust model
+# robust_model = RobustModel(model, 'elliptical', twoTerm = True, gamma = 1)  # generates the robust model
+robust_model = RobustModel(model, 'elliptical', twoTerm = False, gamma = 1)  # generates the robust model
 robust_model_solution = robust_model.robustsolve(verbosity=0)  # solve the robust model
 
 # Below, we are testing the SimPleAC model against failure. The free variables with attribute fixed=True are fixed using
