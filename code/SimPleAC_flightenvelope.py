16bd1aad28d469deac108919b7db929dd4d69e1e
1ozturkbe <1ozturkbe@gmail.com>
2018-10-04 16:20:44 -0400

    3d plotting and flight envelope working.

--- code/SimPleAC_flightenvelope.py
+++ code/SimPleAC_flightenvelope.py
@@ -10,10 +10,14 @@ import numpy as np
 
 # plotting
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 def generate_flight_envelope(m, var1, var2, var1range, rm = None, rmsol = None):
     """
     Generates the flight envelope of an optimized aircraft
+    This is a method to compare the objective tradeoff performance
+    of an already designed aircraft (nominal and robust)
+    Want to MAXIMIZE both var1 and var2 (bigger envelope)
     :param m: already solved model
     :param var1: independent variable
     :param var2: dependent variable (should be maximized)
@@ -25,20 +29,20 @@ def generate_flight_envelope(m, var1, var2, var1range, rm = None, rmsol = None):
     dm = RobustGPTools.DesignedModel(m, m.solution, {})
     if var2.key in m.substitutions.keys():
         del dm.substitutions[var2.key]
-    dm.cost = dm.cost + 1/var2*var2.units*dm.cost.units
+    dm.cost = 1/var2 #*var2.units*dm.cost.units #dm.cost/var2 #
     dm.substitutions.update({var1.key:('sweep',var1range)})
     sol = dm.localsolve(skipsweepfailures=True)
     if rm:
         drm = RobustGPTools.DesignedModel(m, rmsol, {})
         if var2.key in rm.substitutions.keys():
             del drm.substitutions[var2.key]
-        drm.cost = drm.cost + 1/var2*var2.units*drm.cost.units
+        drm.cost = 1/var2 #drm.cost + 1/var2*var2.units*drm.cost.units #drm.cost/var2
         drm.substitutions.update({var1.key:('sweep',var1range)})
         robustsol = drm.localsolve(skipsweepfailures=True)
     plt.plot(mag(sol(var1.key)),mag(sol(var2.key)))
     try:
         plt.plot(mag(robustsol(var1.key)),mag(robustsol(var2.key)))
-        plt.legend("Nominal", "Robust")
+        plt.legend(["Nominal", "Robust"])
     except:
         pass
     plt.xlabel(var1.str_without())
@@ -49,12 +53,22 @@ def generate_flight_envelope(m, var1, var2, var1range, rm = None, rmsol = None):
 
     return sol, robustsol
 
+def plot_general_solutions(solarray, var1, var2, var3):
+    points = ["o","*","-"]
+    count = 0
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for i in solarray:
+        ax.plot(mag(i(var1.key)), mag(i(var2.key)),mag(i(var3.key)))
+        count+=1
+    plt.show()
+
 if __name__ == "__main__":
     m, subs = SimPleAC_setup()
-    # Payload/range feasibility plot for non-robust aircraft
+    # Payload/range feasibility plot for nominal and robust aircraft
     var2 = m['W_{p_m}']
     var1 = m['Range_m']
-    var1range = np.linspace(500,7000,8)*units('km')
+    var1range = np.linspace(500,5000,24)*units('km')
     rm = None
     #plot_feasibilities(x, y, m, rm=rm, design_feasibility=True, skipfailures=True, numberofsweeps=20)
     sol , _, _, directly_uncertain_vars_subs = generate_model_properties(m, 1, 1)  # solving the SimPleAC
@@ -62,4 +76,4 @@ if __name__ == "__main__":
     rmsol = rm.robustsolve()
     sol, robustsol = generate_flight_envelope(m, var1, var2, var1range, rm, rmsol)
 
-
+    plot_general_solutions([sol,robustsol],var1,var2,m['W_{f_m}'])
