4e3e12dfd0151b72e9ad3889b96925fedb7cf130
1ozturkbe <1ozturkbe@gmail.com>
2018-11-01 15:29:45 -0400

    Goal programming draft (needs other method).

--- code/SimPleAC_goal.py
+++ code/SimPleAC_goal.py
@@ -1,7 +1,7 @@
 # This script implements a goal programming approach with SimPleAC
 
 from SimPleAC_setup import SimPleAC_setup
-from gpkit import units, Model, Variable, VectorVariable
+from gpkit import units, Model, Variable, Monomial
 from gpkit.constraints.bounded import Bounded
 from gpkit.small_scripts import mag
 
@@ -11,13 +11,27 @@ from robust.robust_gp_tools import RobustGPTools
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 if __name__ == "__main__":
     m, subs = SimPleAC_setup()
     sol , _, _, directly_uncertain_vars_subs = generate_model_properties(m, 1, 1)  # solving the SimPleAC
-    Gamma = Variable('\Gamma','-','Uncertainty bound')
+    Gamma = Variable('\\Gamma','-','Uncertainty bound')
     solBound = Variable('1+\\delta', '-', 'Acceptable optimal solution bound')
-    m = Model(1/Gamma, [m, m.cost <= sol(m.cost)*solBound, Gamma <= 1e30, solBound <= 1e30], m.substitutions)
-    m.substitutions.update({'1+\\delta':{'sweep':np.linspace(1.05,1.50,10)}})
-    rm = RobustModel(m, 'elliptical', twoTerm=False, gamma=Gamma)
-    rmsol = rm.robustsolve(verbosity=0)  # solve the robust model
+    origcost = m.cost
+    m = Model(1/Gamma, [m, origcost <= Monomial(sol(origcost))*solBound, Gamma <= 1e30, solBound <= 1e30], m.substitutions)
+    ndeltas = 11
+    deltas = np.linspace(2./ndeltas,2.,ndeltas)
+    gammas = np.zeros(ndeltas)
+    costs = np.zeros(ndeltas)
+    hotstart = sol
+    for i in range(ndeltas):
+        m.substitutions.update({'1+\\delta':1+deltas[i]})
+        rm = RobustModel(m, 'elliptical', twoTerm=False, gamma=Gamma, x0=hotstart)
+        rmsol = rm.robustsolve(verbosity=2)  # solve the robust model
+        gammas[i] = 1/mag(rmsol('\\Gamma'))
+        costs[i] = mag(rmsol(origcost))
+        hotstart = rmsol
+
+    plt.plot(gammas,costs)
 
