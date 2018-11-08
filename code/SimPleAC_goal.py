a7d2c0bfc8daf1be97c576b64e4fbacfbb6cb8a0
1ozturkbe <1ozturkbe@gmail.com>
2018-11-08 10:06:40 -0500

    Goal programming giving unexpected results...

--- code/SimPleAC_goal.py
+++ code/SimPleAC_goal.py
@@ -14,24 +14,58 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
+    # Setting up problem
     m, subs = SimPleAC_setup()
     sol , _, _, directly_uncertain_vars_subs = generate_model_properties(m, 1, 1)  # solving the SimPleAC
     Gamma = Variable('\\Gamma','-','Uncertainty bound')
     solBound = Variable('1+\\delta', '-', 'Acceptable optimal solution bound')
     origcost = m.cost
-    m = Model(1/Gamma, [m, origcost <= Monomial(sol(origcost))*solBound, Gamma <= 1e30, solBound <= 1e30], m.substitutions)
-    ndeltas = 11
-    deltas = np.linspace(2./ndeltas,2.,ndeltas)
-    gammas = np.zeros(ndeltas)
-    costs = np.zeros(ndeltas)
+
+    # Solution using standard method of changing Gamma
+    nGammas = 11
+    gammasGammas = np.linspace(1./nGammas,1.,nGammas)
+    costsGammas = np.zeros(nGammas)
     hotstart = sol
+    for i in range(nGammas):
+        rm = RobustModel(m,'elliptical',twoTerm = False, gamma = gammasGammas[i], x0 = hotstart)
+        rmsol = rm.robustsolve(verbosity=2)
+        costsGammas[i] = mag(rmsol(origcost))
+        hotstart = rmsol
+
+    # Goal programming (risk minimization) setup
+    m = Model(1/Gamma, [m, origcost <= Monomial(sol(origcost))*solBound, Gamma <= 1e30, solBound <= 1e30], m.substitutions)
+    ndeltas = nGammas
+    deltas = np.linspace(1.5/ndeltas,1.5,ndeltas)
+    gammasGoal = np.zeros(ndeltas)
+    costsGoal = np.zeros(ndeltas)
+    hotstart = sol
+
+    # Solution using goal programming
     for i in range(ndeltas):
         m.substitutions.update({'1+\\delta':1+deltas[i]})
         rm = RobustModel(m, 'elliptical', twoTerm=False, gamma=Gamma, x0=hotstart)
         rmsol = rm.robustsolve(verbosity=2)  # solve the robust model
-        gammas[i] = 1/mag(rmsol('\\Gamma'))
-        costs[i] = mag(rmsol(origcost))
+        gammasGoal[i] = 1/mag(rmsol('\\Gamma'))
+        costsGoal[i] = mag(rmsol(origcost))
         hotstart = rmsol
 
-    plt.plot(gammas,costs)
+    # Results
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+
+    plt.figure()
+    plt.plot(gammasGammas,costsGammas)
+    plt.plot(gammasGoal, costsGoal)
+    plt.title('Costs with objective minimization and risk minimization')
+    plt.xlabel("Size of uncertainty set, $\Gamma$")
+    plt.show()
+
+    # Odd results
+    plt.figure()
+    plt.plot(deltas, gammasGoal)
+    plt.xlabel('Percent deviation from nominal optimum')
+    plt.ylabel('Size of uncertainty set')
+    plt.title('We capture less uncertainty with more deviation from optimum. Not normal.')
+    plt.show()
+
 
