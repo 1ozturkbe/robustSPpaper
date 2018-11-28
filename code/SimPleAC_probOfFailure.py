1788c632c6fe2b827253f4f5ad517941aa2a7d4a
1ozturkbe <1ozturkbe@gmail.com>
2018-11-28 15:45:42 -0500

    Many changes to probOfFailure (using robust cleanUps2 branch).

--- code/SimPleAC_probOfFailure.py
+++ code/SimPleAC_probOfFailure.py
@@ -1,5 +1,10 @@
 from gpkit import Variable, Model, SignomialsEnabled, units
-from robust.simulations import simulate, read_simulation_data
+from gpkit.small_scripts import mag
+
+from robust.simulations import simulate
+from robust.simulations.simulate import plot_gamma_result_PoFandCost
+from robust.simulations.simulate import filter_gamma_result_dict
+from robust.simulations.read_simulation_data import generate_comparison_plots
 import os
 
 from SimPleAC_setup import SimPleAC_setup
@@ -8,16 +13,16 @@ import numpy as np
 
 if __name__ == '__main__':
     model, subs = SimPleAC_setup()
-    number_of_time_average_solves = 3  # 100
+    number_of_time_average_solves = 1  # 100
     number_of_iterations = 100  # 1000
     nominal_solution, nominal_solve_time, nominal_number_of_constraints, directly_uncertain_vars_subs = \
-        simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations)
+        simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations,'normal')
 
     model_name = 'SimPleAC'
-    nGammas = 3
-    gammas = np.linspace(0.001,1,nGammas)
+    nGammas = 13
+    gammas = np.linspace(0.55,1.15,nGammas)
     min_num_of_linear_sections = 3
-    max_num_of_linear_sections = 99
+    max_num_of_linear_sections = 30
     linearization_tolerance = 1e-3
     verbosity = 1
 
@@ -25,31 +30,62 @@ if __name__ == '__main__':
                {'name': 'Linearized Perturbations', 'twoTerm': False, 'boyd': False, 'simpleModel': False},
                {'name': 'Simple Conservative', 'twoTerm': False, 'boyd': False, 'simpleModel': True}]
     uncertainty_sets = ['box', 'elliptical']
+    # methods = [{'name': 'Best Pairs', 'twoTerm': True, 'boyd': False, 'simpleModel': False}]
+    # uncertainty_sets = ['elliptical']
 
     variable_gamma_file_name = 'simulation_data_variable_gamma.txt'
-    simulate.generate_variable_gamma_results(model, model_name, gammas, number_of_iterations,
+
+    solutions, solve_times, prob_of_failure, avg_cost, number_of_constraints = simulate.variable_gamma_results(
+                                             model, methods, gammas, number_of_iterations,
                                              min_num_of_linear_sections,
                                              max_num_of_linear_sections, verbosity, linearization_tolerance,
-                                             variable_gamma_file_name, number_of_time_average_solves, methods,
-                                             uncertainty_sets, nominal_solution, nominal_solve_time,
-                                             nominal_number_of_constraints, directly_uncertain_vars_subs)
+                                             number_of_time_average_solves,
+                                             uncertainty_sets, nominal_solution, directly_uncertain_vars_subs)
 
-    gamma = 1
-    numbers_of_linear_sections = [40, 60]  # [12, 14, 15, 16, 17, 18, 20, 22, 24, 26, 28, 30, 32, 36, 44, 52, 60, 70, 80]
+    # Plotting of cost and probability of failure for Best Pairs with elliptical uncertainty
+    filteredResult = filter_gamma_result_dict(solutions, 1, 'Best Pairs', 2, 'elliptical')
+    filteredPoF = filter_gamma_result_dict(prob_of_failure, 1, 'Best Pairs', 2, 'elliptical')
+    filteredCosts = filter_gamma_result_dict(avg_cost, 1, 'Best Pairs', 2, 'elliptical')
+    objective_name = 'Total fuel weight'
+    objective_units = 'N'
+    title = ''
+    uncertainty_set = 'elliptical'
+    plot_gamma_result_PoFandCost(title, objective_name, objective_units, filteredResult, filteredPoF, filteredCosts)
 
-    methods = [{'name': 'Best Pairs', 'twoTerm': True, 'boyd': False, 'simpleModel': False},
-               {'name': 'Linearized Perturbations', 'twoTerm': False, 'boyd': False, 'simpleModel': False}]
-    uncertainty_sets = ['box', 'elliptical']
+    #Plotting of solution times
+    gamma = 1.
+    filteredSolutions = filter_gamma_result_dict(solutions, 0, gamma, 2, uncertainty_set)
+    filteredsetup_times = {}
+    for i in filteredSolutions.iterkeys():
+        filteredsetup_times[i] = filteredSolutions[i]['setuptime']
+    filteredsolve_times = filter_gamma_result_dict(solve_times, 0, gamma, 2, uncertainty_set)
+    filteredCosts = filter_gamma_result_dict(avg_cost, 0, gamma, 2, uncertainty_set)
+    filteredn_of_constr = filter_gamma_result_dict(number_of_constraints, 0, gamma, 2, uncertainty_set)
 
-    variable_pwl_file_name = 'simulation_data_variable_pwl.txt'
-    simulate.generate_variable_piecewiselinearsections_results(model, model_name, gamma, number_of_iterations,
-                                                               numbers_of_linear_sections, linearization_tolerance,
-                                                               verbosity, variable_pwl_file_name,
-                                                               number_of_time_average_solves, methods, uncertainty_sets,
-                                                               nominal_solution, nominal_solve_time,
-                                                               nominal_number_of_constraints,
-                                                               directly_uncertain_vars_subs)
+    relative_objective_values = [mag(v) for i,v in sorted(filteredCosts.iteritems())]
+    relative_number_of_constraints = [v/nominal_number_of_constraints for i,v in sorted(filteredn_of_constr.iteritems())]
+    relative_setup_times = [v/nominal_solve_time for v in sorted(filteredsetup_times.iteritems())]
+    relative_solve_times = [v/nominal_solve_time for v in sorted(filteredsolve_times.iteritems())]
 
-    file_path_gamma = 'simulation_data_variable_gamma.txt'
-    file_path_pwl = 'simulation_data_variable_pwl.txt'
-    read_simulation_data.generate_all_plots(file_path_gamma, file_path_pwl)
+    generate_comparison_plots(relative_objective_values, objective_name, relative_number_of_constraints,
+                              relative_setup_times, relative_solve_times, uncertainty_set, methods)
+
+    # gamma = 1
+    # numbers_of_linear_sections = [40, 60]  # [12, 14, 15, 16, 17, 18, 20, 22, 24, 26, 28, 30, 32, 36, 44, 52, 60, 70, 80]
+
+    # methods = [{'name': 'Best Pairs', 'twoTerm': True, 'boyd': False, 'simpleModel': False},
+    #            {'name': 'Linearized Perturbations', 'twoTerm': False, 'boyd': False, 'simpleModel': False}]
+    # uncertainty_sets = ['box', 'elliptical']
+    #
+    # variable_pwl_file_name = 'simulation_data_variable_pwl.txt'
+    # simulate.generate_variable_piecewiselinearsections_results(model, model_name, gamma, number_of_iterations,
+    #                                                            numbers_of_linear_sections, linearization_tolerance,
+    #                                                            verbosity, variable_pwl_file_name,
+    #                                                            number_of_time_average_solves, methods, uncertainty_sets,
+    #                                                            nominal_solution, nominal_solve_time,
+    #                                                            nominal_number_of_constraints,
+    #                                                            directly_uncertain_vars_subs)
+    #
+    # file_path_gamma = 'simulation_data_variable_gamma.txt'
+    # file_path_pwl = 'simulation_data_variable_pwl.txt'
+
