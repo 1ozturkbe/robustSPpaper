39e6d8e50d86f9340d0b9ccb3ea2303a79c218ee
1ozturkbe <1ozturkbe@gmail.com>
2018-12-31 07:39:56 -0600

    Goal programming complete.

--- code/SimPleAC_goal.py
+++ code/SimPleAC_goal.py
@@ -9,92 +9,101 @@ from robust.robust import RobustModel
 from robust.simulations.simulate import generate_model_properties
 from robust.robust_gp_tools import RobustGPTools
 from robust.simulations.simulate import simulate_robust_model
+import robust.simulations.simulate as simulate
+from robust.simulations.simulate import plot_gamma_result_PoFandCost
+from robust.simulations.simulate import filter_gamma_result_dict
 
 import numpy as np
 
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    # Setting up problem
-    m, subs = SimPleAC_setup()
-    number_of_time_average_solves = 10
-    number_of_iterations = 200
-    sol, _, _, directly_uncertain_vars_subs = generate_model_properties(m, number_of_time_average_solves,
-                                                                        number_of_iterations)  # solving the SimPleAC
-    Gamma = Variable('\\Gamma', '-', 'Uncertainty bound', fix = True)
-    solBound = Variable('1+\\delta', '-', 'Acceptable optimal solution bound', fix = True)
-    origcost = m.cost
+from SimPleAC_save import save_obj, load_obj
+import cPickle as pickle
 
-    # Robust models setup
-    linearization_tolerance = 1e-3
+if __name__ == "__main__":
+    model, subs = SimPleAC_setup()
+    number_of_time_average_solves = 3
+    number_of_iterations = 150
+    nominal_solution, nominal_solve_time, nominal_number_of_constraints, directly_uncertain_vars_subs = \
+        simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations,'normal')
+
+    model_name = 'SimPleAC'
+    nGammas = 16
+    gammas = np.linspace(0.8,1.05,nGammas)
     min_num_of_linear_sections = 3
     max_num_of_linear_sections = 99
-    verbosity = 0
-    nominal_solution = sol
-    methods = [{'name': 'Best Pairs', 'twoTerm': True, 'boyd': False, 'simpleModel': False},
-               {'name': 'Linear. Perts.', 'twoTerm': False, 'boyd': False, 'simpleModel': False},
-               {'name': 'Simple Cons.', 'twoTerm': False, 'boyd': False, 'simpleModel': True},
-               {'name': 'Two Term', 'twoTerm': False, 'boyd': True, 'simpleModel': False}]
-    method = methods[0]
+    linearization_tolerance = 1e-4
+    verbosity = 1
 
-    # Solution using standard method of changing Gamma
-    nGammas = 15
-    gammasGammas = np.linspace(0.85, 1.35, nGammas)
-    costsGammas = np.zeros(nGammas)
-    poFGammas = np.zeros(nGammas)
-    hotstart = sol
-    for i in range(nGammas):
-        rm, rmsol, rmsoltime, simulation_results = simulate_robust_model(m, method, 'elliptical', gammasGammas[i],
-                                                                         directly_uncertain_vars_subs,
-                                                                         number_of_iterations, linearization_tolerance,
-                                                                         min_num_of_linear_sections,
-                                                                         max_num_of_linear_sections, verbosity,
-                                                                         nominal_solution,
-                                                                         number_of_time_average_solves)
-        costsGammas[i] = mag(rmsol(origcost))
-        poFGammas[i] = simulation_results[0]
+    methods = [{'name': 'Best Pairs', 'twoTerm': True, 'boyd': False, 'simpleModel': False}]
+    uncertainty_sets = ['elliptical']
+
+    #Solution using standard method of changing Gamma
+    gamma = {}
+    gamma['solutions'], gamma['solve_times'], gamma['simulation_results'], \
+    gamma['number_of_constraints'] = simulate.variable_gamma_results(
+                                             model, methods, gammas, number_of_iterations,
+                                             min_num_of_linear_sections,
+                                             max_num_of_linear_sections, verbosity, linearization_tolerance,
+                                             number_of_time_average_solves,
+                                             uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel=False)
+
+    # Saving results
+    for i,v in gamma['solutions'].iteritems():
+        v.save('goalResults/gamma'+ str(i))
+    save_obj(gamma['solve_times'], 'gammasolve_times', 'goalResults')
+    save_obj(gamma['simulation_results'], 'gammasimulation_results', 'goalResults')
+    save_obj(gamma['number_of_constraints'], 'gammanumber_of_constraints', 'goalResults')
+
+    # # Loading results
+    # gamma = {}
+    # gamma['solutions'] = {}
+    # for i in range(nGammas):
+    #     gamma['solutions'][gammas[i], methods[0]['name'], uncertainty_sets[0]] = pickle.load(open("goalResults\gamma" +
+    #                                                                 str((gammas[i], methods[0]['name'], uncertainty_sets[0]))))
+    # gamma['solve_times'] = load_obj('gammasolve_times', 'goalResults')
+    # gamma['simulation_results'] = load_obj('gammasimulation_results', 'goalResults')
+    # gamma['number_of_constraints'] = load_obj('gammanumber_of_constraints', 'goalResults')
 
     # Goal programming (risk minimization) setup
-    mGoal = Model(1 / Gamma, [m, origcost <= Monomial(sol(origcost)) * solBound, Gamma <= 1e30, solBound <= 1e30],
-                  m.substitutions)
-    mGoal.substitutions.update({'1+\\delta': 1 + 1e-2})
-    nominal_solution = mGoal.localsolve()
     ndeltas = nGammas
-    deltas = np.linspace(0.001, 2.2, ndeltas)
-    gammasGoal = np.zeros(ndeltas)
-    costsGoal = np.zeros(ndeltas)
-    poFGoal = np.zeros(ndeltas)
-    hotstart = sol
+    deltas = np.linspace(0.001, 1.0, ndeltas)
+    hotstart = nominal_solution
 
     # Solution using goal programming
-    for i in range(ndeltas):
-        mGoal.substitutions.update({'1+\\delta': 1 + deltas[i]})
-        rm, rmsol, rmsoltime, simulation_results = simulate_robust_model(mGoal, method, 'elliptical', Gamma,
-                                                                         directly_uncertain_vars_subs,
-                                                                         number_of_iterations, linearization_tolerance,
-                                                                         min_num_of_linear_sections,
-                                                                         max_num_of_linear_sections, verbosity,
-                                                                         nominal_solution,
-                                                                         number_of_time_average_solves)
-        gammasGoal[i] = mag(rmsol('\\Gamma'))
-        costsGoal[i] = mag(rmsol(origcost))
-        poFGoal[i] = simulation_results[0]
+    delta = {}
+    delta['solutions'], delta['solve_times'], delta['simulation_results'], \
+    delta['number_of_constraints'] = simulate.variable_goal_results(
+                                             model, methods, deltas, number_of_iterations,
+                                             min_num_of_linear_sections,
+                                             max_num_of_linear_sections, verbosity, linearization_tolerance,
+                                             number_of_time_average_solves,
+                                             uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel=False)
 
-    #
-    # Results
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
 
-    plt.figure()
-    plt.plot(gammasGammas, costsGammas)
-    plt.plot(gammasGoal, costsGoal)
-    plt.xlabel("Size of uncertainty set, $\Gamma$")
-    plt.ylabel("Normalized fuel burn")
-    plt.show()
+    # # Saving goal programmingresults
+    for i,v in delta['solutions'].iteritems():
+        v.save('goalResults/delta'+ str(i))
+    save_obj(delta['solve_times'], 'deltasolve_times', 'goalResults')
+    save_obj(delta['simulation_results'], 'deltasimulation_results', 'goalResults')
+    save_obj(delta['number_of_constraints'], 'deltanumber_of_constraints', 'goalResults')
 
-    plt.figure()
-    plt.plot(gammasGammas, poFGammas)
-    plt.plot(gammasGoal, poFGoal)
-    plt.xlabel("Size of uncertainty set, $\Gamma$")
-    plt.ylabel("Probability of failure")
-    plt.show()
+    # # Loading goal programming results
+    # delta = {}
+    # delta['solutions'] = {}
+    # for i in range(ndeltas):
+    #     delta['solutions'][deltas[i], methods[0]['name'], uncertainty_sets[0]] = pickle.load(open("goalResults\delta" +
+    #                                                                 str((deltas[i], methods[0]['name'], uncertainty_sets[0]))))
+    # delta['solve_times'] = load_obj('deltasolve_times', 'goalResults')
+    # delta['simulation_results'] = load_obj('deltasimulation_results', 'goalResults')
+    # delta['number_of_constraints'] = load_obj('deltanumber_of_constraints', 'goalResults')
+
+    # Plotting of cost and probability of failure for Best Pairs with elliptical uncertainty
+    for i in [delta, gamma]:
+        filteredResult = filter_gamma_result_dict(i['solutions'], 1, 'Best Pairs', 2, 'elliptical')
+        filteredSimulations = filter_gamma_result_dict(i['simulation_results'], 1, 'Best Pairs', 2, 'elliptical')
+        objective_name = 'Total fuel weight'
+        objective_units = 'N'
+        title = ''
+        uncertainty_set = 'elliptical'
+        plot_gamma_result_PoFandCost(title, objective_name, objective_units, filteredResult, filteredSimulations)
