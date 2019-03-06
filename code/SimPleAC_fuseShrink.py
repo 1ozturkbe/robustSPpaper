fcee3086d7102e5b9c6cc4b472ec088c894fee7c
1ozturkbe <1ozturkbe@gmail.com>
2019-03-06 10:26:05 -0500

    Fuselage shrinkage figure.

--- code/SimPleAC_fuseShrink.py
+++ code/SimPleAC_fuseShrink.py
@@ -21,14 +21,13 @@ if __name__ == '__main__':
     model.cost = model['W_{f_m}'] + model['C_m'] * model['t_m'] * units('N')
     number_of_time_average_solves = 3  # 100
     number_of_iterations = 100  # 1000
-    # nominal_solution, nominal_solve_time, nominal_number_of_constraints, directly_uncertain_vars_subs = \
-    #     simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations, 'normal')
+
     nominal_solution, nominal_solve_time, nominal_number_of_constraints, directly_uncertain_vars_subs = \
-        simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations)
+        simulate.generate_model_properties(model, number_of_time_average_solves, number_of_iterations, 'normal')
 
     model_name = 'SimPleAC'
-    nGammas = 3
-    gammas = np.linspace(0.05, 1.05, nGammas)
+    nGammas = 10
+    gammas = np.linspace(0.1, 1.0, nGammas)
     min_num_of_linear_sections = 3
     max_num_of_linear_sections = 99
     linearization_tolerance = 1e-4
@@ -38,33 +37,12 @@ if __name__ == '__main__':
 
     for i in gammas:
         nm = RobustModel(model, 'elliptical', twoTerm=True, boyd=False, simpleModel=False, gamma=i)
-        solutions.append(nm.robustsolve())
+        solutions.append(nm.robustsolve(verbosity = 2))
 
-    # solutions, solve_times, simulation_results, number_of_constraints = simulate.variable_gamma_results(
-    #                                          model, methods, gammas, number_of_iterations,
-    #                                          min_num_of_linear_sections,
-    #                                          max_num_of_linear_sections, verbosity, linearization_tolerance,
-    #                                          number_of_time_average_solves,
-    #                                          uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel=False)
-
-    # Saving results
-    # for i,v in solutions.iteritems():
-    #     v.save('fuseShrink/'+ str(i))
-    # save_obj(solve_times, 'fusesolve_times', 'fuseResults')
-    # save_obj(simulation_results, 'fusesimulation_results', 'fuseResults')
-    # save_obj(number_of_constraints, 'fusenumber_of_constraints', 'fuseResults')
-    #
-    # # Plotting of cost and probability of failure for Best Pairs with elliptical uncertainty
-    # filteredResult = filter_gamma_result_dict(solutions, 1, 'Best Pairs', 2, 'elliptical')
-    # filteredSimulations = filter_gamma_result_dict(simulation_results, 1, 'Best Pairs', 2, 'elliptical')
-    # objective_name = 'Total fuel weight'
-    # objective_units = 'N'
-    # title = ''
-    # uncertainty_set = 'elliptical'
-    # plot_gamma_result_PoFandCost(title, objective_name, objective_units, filteredResult, filteredSimulations)
-
-
+    SimPleAC_draw(nominal_solution, color='blue', directory = 'fuseResults/', name='fuse0')
+    count = int(0)
     for i in solutions:
-        print i('S')
+        print i('S'), i['cost']
     for i in solutions:
-        SimPleAC_draw(i, color='blue')
+        count+=1
+        SimPleAC_draw(i, color='blue', directory = 'fuseResults/', name='fuse'+str(count))
