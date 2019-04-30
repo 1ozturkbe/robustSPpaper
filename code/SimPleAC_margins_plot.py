6c17dc1feda7b13ec1bd53a4f3debf08d6d05a09
1ozturkbe <1ozturkbe@gmail.com>
2019-04-30 16:50:18 -0400

    Simulating and plotting margins.

---
+++ code/SimPleAC_margins_plot.py
@@ -0,0 +1,48 @@
+# This script implements a goal programming approach with SimPleAC
+from robust.simulations.simulate import plot_goal_result_PoFandCost
+from robust.simulations.simulate import filter_gamma_result_dict
+from robust.simulations.read_simulation_data import objective_proboffailure_vs_gamma
+
+import numpy as np
+from SimPleAC_save import save_obj, load_obj
+from SimPleAC_pof_simulate import pof_parameters
+import cPickle as pickle
+from gpkit.small_scripts import mag
+
+
+if __name__ == "__main__":
+    # Retrieving pof parameters
+    [model, methods, gammas, number_of_iterations,
+    min_num_of_linear_sections, max_num_of_linear_sections, verbosity, linearization_tolerance,
+    number_of_time_average_solves, uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel,
+            nominal_number_of_constraints, nominal_solve_time] = pof_parameters()
+
+    # # Loading margins results
+    margin = {}
+    nmargins = len(gammas)
+    margins = np.linspace(0.,1.,nmargins)
+
+    margin['solutions'] = {}
+    for i in range(nmargins):
+        margin['solutions'][margins[i]] = pickle.load(open("marginResults/" +
+                                                                    str(margins[i])))
+    margin['number_of_constraints'] = load_obj('marginnumber_of_constraints', 'marginResults')
+    margin['simulation_results'] = load_obj('marginsimulation_results', 'marginResults')
+
+    # Plotting of cost and probability of failure
+    objective_name = 'Total fuel weight'
+    objective_units = 'N'
+    title = ''
+    filteredResult = margin['solutions']
+    filteredSimulations = margin['simulation_results']
+    objective_varkey = model.cost.key
+
+    objective_costs = []
+    pofs = []
+    objective_stddev = []
+    for i in sorted(filteredResult.keys()):
+        objective_stddev.append(filteredSimulations[i][2])
+        objective_costs.append(mag(filteredResult[i](objective_varkey)))
+        pofs.append(filteredSimulations[i][0])
+    objective_proboffailure_vs_gamma(margins, objective_costs, objective_name, objective_units,
+                                     np.min(objective_costs), np.max(objective_costs), pofs, title, objective_stddev)
---
+++ code/SimPleAC_margins_simulate.py
@@ -0,0 +1,52 @@
+import numpy as np
+from SimPleAC_setup import SimPleAC_setup
+from SimPleAC_save import save_obj
+from gpkit.small_scripts import mag
+from gpkit import Model
+
+from SimPleAC_pof_simulate import pof_parameters
+from robust.simulations.simulate import RobustGPTools
+import cPickle as pickle
+
+if __name__ == '__main__':
+    [model, methods, gammas, number_of_iterations,
+    min_num_of_linear_sections, max_num_of_linear_sections, verbosity, linearization_tolerance,
+    number_of_time_average_solves, uncertainty_sets, nominal_solution, directly_uncertain_vars_subs, parallel,
+    nominal_number_of_constraints, nominal_solve_time] = pof_parameters()
+
+    # Loading directly_uncertain_vars_subs
+    try:
+        pickle_in = open('directly_uncertain_dict.pickle', 'r')
+        directly_uncertain_vars_subs = pickle.load(pickle_in)
+        pickle_in.close()
+    except:
+        print 'Warning: Please run pof_simulate first for consistent MC results.'
+
+
+    nGammas = len(gammas)
+    solutions = {}
+    simulation_results = {}
+    number_of_constraints = {}
+    for i in range(nGammas):
+        print i
+        margin_subs = {k: v + gammas[i]*np.sign(mag(nominal_solution['sensitivities']['constants'](k.key)))*k.key.pr * v / 100.0
+                                     for k, v in model.substitutions.items()
+                                     if k in model.varkeys and RobustGPTools.is_directly_uncertain(k)}
+
+        mm = Model(model.cost, model)
+        mm.substitutions.update(margin_subs)
+        solutions[gammas[i]] = mm.localsolve(verbosity=0)
+        simulation_results[gammas[i]] = RobustGPTools.probability_of_failure(mm, solutions[gammas[i]], directly_uncertain_vars_subs,
+                                                                             number_of_iterations)
+        try:
+            number_of_constraints[gammas[i]] = len([cnstrnt for cnstrnt in mm.flat(constraintsets=False)])
+        except AttributeError:
+            number_of_constraints[gammas[i]] = len([cnstrnt for cnstrnt in mm[-1].flat(constraintsets=False)])
+        print simulation_results[gammas[i]]
+
+    # Saving results
+    for i,v in solutions.iteritems():
+        v.save('marginResults/'+ str(i))
+    save_obj(number_of_constraints, 'marginnumber_of_constraints', 'marginResults')
+    save_obj(simulation_results, 'marginsimulation_results', 'marginResults')
+
